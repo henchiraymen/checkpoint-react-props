5554cfef48544a8c0be2cdf75f0c62c94707ecee
Aymen <hench.aymen@gmail.com>
2021-11-15 10:23:53 +0100

    adding default value, propsType and inline style

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import "./App.css";
 import Profile from "./profile/Profile.js";
 
 function App() {
-  const handleName = (name) => alert(name);
+  const handleName = (name) => alert(`My name is : ${name}`);
   return (
     <div>
       <Profile
         fullName="Aymen Henchir"
         bio="developing, and implementing applications and solutions using a range of technologies and programming languages."
         profession="Junior Full Stack JS Developer"
-        handleName={handleName(`My name is : ` + Profile.fullName)}
+        handleName={handleName}
       >
         <img src="/photo.jpg" alt="aymen henchir" />
       </Profile>
--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -1,15 +1,39 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Profile = (props) => {
+  // Inline Style Object
+  const styleObject = {
+    backgroundColor: "orange",
+    height: "30px",
+    border: "black",
+    borderRadius: "10%",
+  };
   return (
     <div className="App">
       <h2> {props.children}</h2>
       <h2> fullName : {props.fullName} </h2>
       <h2> Bio : {props.bio} </h2>
       <h2> Profession : {props.profession} </h2>
-      <button onClick={() => props.handleName}>ClickMe</button>
+      <button
+        style={styleObject}
+        onClick={() => props.handleName(props.fullName)}
+      >
+        ClickMe
+      </button>
     </div>
   );
 };
-
+//Default value of props
+Profile.defaultProps = {
+  fullName: "unknown",
+  bio: "unknown",
+  profession: "unknown",
+};
+//check Props Type.
+Profile.propTypes = {
+  fullName: PropTypes.string,
+  bio: PropTypes.string,
+  profession: PropTypes.string,
+};
 export default Profile;
